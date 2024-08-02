d87c5507392994072091b156d3918e4513c18b37
Shahid Raza <aizansami123@gmail.com>
2024-08-03 03:41:09 +0530

    done

--- time-zone/time-zone-convert/src/components/TimezoneSlider.jsx
+++ time-zone/time-zone-convert/src/components/TimezoneSlider.jsx
@@ -5,10 +5,20 @@ import "rc-slider/assets/index.css";
 import moment from "moment-timezone";
 import { AiOutlineClose } from "react-icons/ai";
 import "./TimezoneSlider.scss";
+import { RxBorderDotted } from "react-icons/rx";
 
 const TimezoneSlider = ({ timezone, baseTime, onChange, onRemove }) => {
   const handleSliderChange = (value) => {
-    const newTime = moment(baseTime).startOf("day").add(value, "hours");
+    // Clamp value to between 0 and 24 hours
+    const clampedValue = Math.min(Math.max(value, 0), 24);
+
+    let newTime = moment(baseTime).startOf("day").add(clampedValue, "hours");
+
+    // Check if the slider is at the end of the day and set the time to 24:00 if needed
+    if (clampedValue === 24) {
+      newTime = moment(baseTime).endOf("day");
+    }
+
     onChange(newTime);
   };
 
@@ -17,6 +27,13 @@ const TimezoneSlider = ({ timezone, baseTime, onChange, onRemove }) => {
   return (
     <div className="timezone-slider">
       <div className="timezone-box">
+        <RxBorderDotted />
+        <RxBorderDotted />
+        <RxBorderDotted />
+        <br />
+        <RxBorderDotted />
+        <RxBorderDotted />
+        <br />
         <h3>{timezone}</h3>
         <button
           className="remove-btn"
@@ -42,6 +59,7 @@ const TimezoneSlider = ({ timezone, baseTime, onChange, onRemove }) => {
             21: "9pm",
             24: "12am",
           }}
+          style={{ width: "100%" }}
           included={false}
         />
         <p>{timeInZone.format("h:mm A, dddd, MMM D")}</p>
